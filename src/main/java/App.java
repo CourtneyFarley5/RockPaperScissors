70599d088dd09bdb382c80955afdf44e42063979
CourtneyFarley <c.farley@corecomtechacademy.co.uk>
2024-05-21 16:29:29 +0100

    Update2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) {
+        Choices user = new Choices();
+
+
+        //tried multiple times to intoduce a class and object between choices and here
+        //took up all of my time and still didn't figure it out.
+
 
     }
 }
